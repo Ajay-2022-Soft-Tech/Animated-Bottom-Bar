ec4bd7c87eff8c49a7dec1de8e00790bb36aaee7
Ajay Dewangan <dewanganajay05@gmail.com>
2023-07-21 00:16:06 +0530

    Animated Bottom Navigation bar in Android

---
+++ app/src/main/java/com/example/animatedbottomnavigationneatroots/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.animatedbottomnavigationneatroots
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Layout
+import androidx.compose.foundation.layout.Arrangement
+import java.lang.reflect.Modifier
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
